9e29e65cfb3817039e4f5ddad0eac6d830ee3d1f
sulin <sulinixl@163.com>
2019-06-19 21:28:48 +0800

    route qt's log to slf4go

--- core/goxui_core.go
+++ core/goxui_core.go
@@ -15,8 +15,37 @@ package core
 #cgo darwin LDFLAGS: -lc++
 
 #cgo LDFLAGS: -lgoxui
+#include "goxui.h"
+
+extern void logger(int l, char *msg);
+
+static inline void bindLogger() {
+	ui_set_logger(logger);
+}
 */
 import "C"
 import "github.com/go-eden/slf4go"
 
 var log = slog.GetLogger()
+
+func init() {
+	C.bindLogger()
+}
+
+//export logger
+func logger(cLevel C.int, cMsg *C.char) {
+	l := int(cLevel)
+	msg := C.GoString(cMsg)
+	switch l {
+	case 0:
+		log.Debug(msg)
+	case 1:
+		log.Warn(msg)
+	case 2:
+		log.Error(msg)
+	case 3:
+		log.Fatal(msg)
+	case 4:
+		log.Infof(msg)
+	}
+}
