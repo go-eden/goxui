bfb28811b82def713940124bee0a2c6c3e247e28
sulin <sulinixl@163.com>
2019-06-20 13:38:18 +0800

    return exit code

--- goxui_tool.go
+++ goxui_tool.go
@@ -6,10 +6,10 @@ import (
 )
 
 // Start UI from the specified relative path
-func StartRelative(rpath ...string) {
+func StartRelative(rpath ...string) int {
 	_, filename, _, _ := runtime.Caller(1)
 
 	path := filepath.Dir(filename)
 
-	Start(filepath.Join(path, filepath.Join(rpath...)))
+	return Start(filepath.Join(path, filepath.Join(rpath...)))
 }
